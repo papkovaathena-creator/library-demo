114c60f2868bc63c3ae1e1f9e3c14ac200ad5a3e
Papkova Athena <papkovaathena@gmail.com>
2026-04-06 22:32:02 +0200

    Switched project to Gradle

--- Backend.Dockerfile
+++ Backend.Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:17.0.1-jdk-slim
-ARG jar-file=target/*.jar
+#ARG jar-file=target/*.jar
+ARG jar-file=build/libs/library-demo-0.0.1-SNAPSHOT.jar
 COPY ./target/library-demo-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/ru/athena/library_demo/LibraryDemoApplicationTests.java
+++ src/test/java/ru/athena/library_demo/LibraryDemoApplicationTests.java
@@ -2,8 +2,10 @@ package ru.athena.library_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("hsqldb")
 class LibraryDemoApplicationTests {
 
 	@Test
